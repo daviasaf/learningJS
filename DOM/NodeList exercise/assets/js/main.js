3b3a8018e04a358607443ab73edef750360b2785
Davi Asaf <148470155+DaviAsaf@users.noreply.github.com>
2025-02-24 19:14:37 -0300

    Update main.js

--- DOM/NodeList exercise/assets/js/main.js
+++ DOM/NodeList exercise/assets/js/main.js
@@ -5,7 +5,7 @@ const estilosBody = getComputedStyle(document.body)
 const backgroundColor = estilosBody.backgroundColor;
 
 for(p of ps){
-    p.style.backgroundColor = "rgb(17, 86, 102)"
+    p.style.backgroundColor = backgroundColor
     p.style.padding = "10px"
     p.style.color = "white"
 }
